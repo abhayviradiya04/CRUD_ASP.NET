0f00e54bb2219143196bea253689a9d7cd9de5bd
Abhay Viradiya <121176886+abhayviradiya04@users.noreply.github.com>
2025-07-27 09:37:41 +0530

    done

--- CRUD_Demo/obj/Debug/net8.0/CRUD_Demo.AssemblyInfo.cs
+++ CRUD_Demo/obj/Debug/net8.0/CRUD_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51ef8d3f4f178b31439ed5db0bcd3df17ea4509")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
